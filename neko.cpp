aa563175151fed9d60e88d4f021b58800ae59534
Neko Nik <nikhil@nekonik.com>
2024-06-28 22:31:25 +0530

    chore: Add initial implementation of Neko Nik C++ API test file

---
+++ neko.cpp
@@ -0,0 +1,32 @@
+/*
+ * Commercial Standard License
+ * 
+ * This file is part of the Neko Nik C++ API.
+ * 
+ * This software is licensed under the Commercial Standard License.
+ * 
+ * You may not redistribute or resell the software.
+ * Commercial or educational use is NOT allowed.
+ * You are allowed to have this software installed on 3 machine(s) at once.
+ * 
+ * This product does NOT come with any warranty, express or implied.
+ * 
+ * The full license can be found in the LICENSE file included with this project.
+ * 
+ * © Neko Nik <https://www.nekonik.com/impressum>
+ * 
+ * Contact: admin@nekonik.com
+ */
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Neko Nik!" << std::endl;
+    return 0;
+}
+
+// Compile with: g++ neko.cpp -o neko
+// Run with: ./neko
+
+// This file is used to test any new features or changes to the Neko Nik C++ API.
